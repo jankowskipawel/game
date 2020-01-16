c918366808201c47d50bb865a46e39752c60c91a
jankowskipawel <paweljankowski246@gmail.com>
2020-01-17 00:25:00 +0100

    Initial commit, add player movement and camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                _navMeshAgent.destination = hit.point;
+            }
+        }
+    }
+}
