188e0dd4b20762e9ee01bd4e841624966c2f835b
jankowskipawel <paweljankowski246@gmail.com>
2020-01-17 15:06:56 +0100

    Change player model and add animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,30 @@ using UnityEngine.AI;
 public class PlayerMovement : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
+    private GameObject _player;
+    private Animator _animator;
+
+    private bool _isRunning = false;
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+
+
     // Start is called before the first frame update
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _player = GameObject.Find("Player");
+        _animator = _player.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        MovePlayer(ray);
+    }
 
+    public void MovePlayer(Ray ray)
+    {
         RaycastHit hit;
         if (Input.GetMouseButton(0))
         {
@@ -25,5 +38,14 @@ public class PlayerMovement : MonoBehaviour
                 _navMeshAgent.destination = hit.point;
             }
         }
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+        {
+            _isRunning = false;
+        }
+        else
+        {
+            _isRunning = true;
+        }
+        _animator.SetBool(IsRunning,_isRunning);
     }
 }
