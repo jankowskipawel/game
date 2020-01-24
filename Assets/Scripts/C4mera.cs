235d8efc8dfa0e26d4519b780204bdcb1798a1ad
Pawel MacOS <paweljankowski246@gmail.com>
2020-01-24 23:43:19 +0100

    Add resource popup script, change tree script to gather script to make it more general

---
+++ Assets/Scripts/C4mera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C4mera : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraHeight = 20.0f;
+    public float cameraDistance = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.y += cameraHeight;
+        pos.z -= cameraDistance;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Resource : MonoBehaviour
+{
+    private GameObject _player;
+    private PlayerMovement _playerMovement;
+    private NavMeshAgent _playerNavMeshAgent;
+    private Mesh originalMesh;
+    public Mesh depletedMesh;
+    private MeshFilter _meshFilter;
+    private RespawnResource _respawnResource;
+    
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+        _playerMovement = _player.GetComponent<PlayerMovement>();
+        _playerNavMeshAgent = _player.GetComponent<NavMeshAgent>();
+        originalMesh = gameObject.GetComponent<Mesh>();
+        _meshFilter = gameObject.GetComponent<MeshFilter>();
+        _respawnResource = transform.parent.GetComponent<RespawnResource>();
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnMouseDown()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(_player.transform.position, 3f);
+        bool isNearObject = false;
+        foreach (var collider in hitColliders)
+        {
+            if (collider == gameObject.GetComponent<Collider>())
+            {
+                isNearObject = true;
+            }
+        }
+
+        if (!isNearObject)
+        {
+            var targetPos = _playerMovement.MovePlayerToObjPos(gameObject, 0.8f);
+            var coroutine = WaitAndCut(targetPos);
+            StartCoroutine(coroutine);
+        }
+        else
+        {
+            Gather();
+        }
+        
+    }
+    
+    IEnumerator WaitAndCut(Vector3 targetpos)
+    {
+        float timeToReach = (_playerMovement.CalculatePathLength(targetpos)) / (_playerNavMeshAgent.speed-1);
+        yield return new WaitForSeconds(timeToReach);
+        if (_playerMovement.IsPathCompleted())
+        {
+            Gather();
+        }
+    }
+    
+    
+
+    private void Gather()
+    {
+        gameObject.SetActive(false);
+        _playerNavMeshAgent.ResetPath();
+        _respawnResource.StartRespawn();
+    }
+
+    
+    
+}
