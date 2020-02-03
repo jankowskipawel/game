b22dfd620acad6c0f12d2cce473c468eda15e7c8
jankowskipawel <paweljankowski246@gmail.com>
2020-02-03 03:01:48 +0100

    Add simple inventory UI, remove unused materials and objects

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool isActive = false;
+    public GameObject inventoryUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (isActive)
+            {
+                inventoryUI.SetActive(false);
+                isActive = false;
+            }
+            else
+            {
+                inventoryUI.SetActive(true);
+                isActive = true;
+            } 
+        }
+    }
+}
